c6c4d158c4bc061a55a676ed9212a73db50925e9
ituchkub <ituchkub@hotmail.com>
2022-08-22 14:42:56 +0700

    Last

--- index.js
+++ index.js
@@ -14,16 +14,20 @@ app.get('/', (req, res) => {
     res.send('This is my API running...')
 })
 
+//https://node-api-vercel-ecru.vercel.app/get/ITuCHKub
 app.get('/get/:name', (req, res) => {
     let name = req.params.name;
-    res.send('This is my E-mail : ' + name )
+    res.send('This is my Name  : ' + name )
 })
 
+//{  "Name": "ituchkub",    "Mail": "ituchkub@gmail.com" }
 app.post('/post', (req, res) => {
 
-   // console.log(req.body);
-   // let name = req.body.name;
-    res.send(req.body)
+   
+   let name = req.body.name;
+   let Mail = req.body.Mail;
+   res.send('This is my Name  : ' + name+' And My E-Mail : '+Mail )
+   // res.send(req.body)
 })
 
 module.exports = app
