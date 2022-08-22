08e2da61b5e2d21085d6a7e594f91c194aec7370
ituchkub <ituchkub@hotmail.com>
2022-08-22 14:32:13 +0700

    0

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 4000
 
-
+app.configure(function(){
+    app.use(express.bodyParser());
+  });
 
 app.listen(PORT, () => {
     console.log(`API Listening on PORT ${PORT}`)
